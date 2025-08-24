8f7ded59eaf6bb8ba2b15f7af426a5387ffa1b30
Fatima Bhatti <95415789+ammar1zahid@users.noreply.github.com>
2025-08-24 18:41:41 +0500

    added window.location.origin for redirect

--- app/components/dashboard/sidebar/sidebar.jsx
+++ app/components/dashboard/sidebar/sidebar.jsx
@@ -66,7 +66,7 @@ export default function Sidebar() {
   const user = session?.user;
   const handleSignOut = async () => {
     try {
-      await signOut({ callbackUrl: "/login" });
+      await signOut({ callbackUrl: window.location.origin + "/login" });
     } catch (err) {
       console.error("signOut error:", err);
     }
